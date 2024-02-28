9537e55063f1d4b120f6dbcf3f6ab942056a0e52
Amit Sharma <iamamitshrma@gmail.com>
2024-02-29 00:09:12 +0530

    fix api folder

--- app/api/user/register/route.ts
+++ app/api/user/register/route.ts
@@ -1,10 +1,23 @@
 import { connectToDB } from "@/lib/database";
+import { User } from "@/models/users.model";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
     try {
         connectToDB();
         const { username, email, fullName, avatar, password } = await req.json();
+
+        if ([fullName, email, username, password].some((field) => field?.trim() === "")) {
+            return NextResponse.json({message: "All fields are required!"}, {status: 400})
+        }
+
+        const existedUser = await User.findOne({
+            $or: [{ username }, { email }]
+        });
+        if (existedUser) {
+            return NextResponse.json({message: "User with email or username already exists!"}, {status: 400})
+        }
+        
         console.log({username, email, fullName, avatar, password})
         return NextResponse.json({message: "User Registered successfully!", data: null}, {status: 200})
     } catch (error) {
